c2dfdda724fbb36ab366354bf85a3a5d0da1e991
Rain <smalltime153@gmail.com>
2021-03-26 15:16:05 +0800

    🤔 feat: add user model

---
+++ src/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { ApiProperty, ApiTags, ApiOperation } from '@nestjs/swagger';
+import { UserService } from './user.service';
+import { User as UserModel, Post as PostModel } from '@prisma/client';
+
+class userDto {
+  @ApiProperty({ description: '用户名' })
+  username?: string;
+  @ApiProperty({ description: '密码' })
+  password: string;
+  @ApiProperty({ description: '邮箱' })
+  email?: string;
+  @ApiProperty({ description: '手机号' })
+  phone?: string;
+}
+
+@ApiTags('用户管理')
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @ApiOperation({ summary: '注册用户' })
+  @Post()
+  async signupUser(@Body() userData: userDto): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
